a44e662bf9100c8f281af53b2662437c9ed240c4
izot <evgeny2900@gmail.com>
2015-06-27 13:12:04 +0300

    desktoppr.co random wallpaper installer

--- run_all.py
+++ run_all.py
@@ -2,15 +2,18 @@
 # -*- coding: utf-8 -*-
 import sys
 import os
+from urllib2 import urlopen
+import json
 from main2 import Cmd_manager
 from main2 import Gui_manager
 
+
 class All_manager(Cmd_manager):
-
-
     def __init__(self):
         self.YOUTUBE = "youtube-dl -o \"{folder}%(title)s.%(ext)s\" {url}"
         self.PYREVERSE = "pyreverse -my -A -o png -p {name} {file}"
+        self.WGET = "wget {url} -P {folder}"
+        self.FEH = "feh --bg-scale {pathimg}"
 
     def exe_youtube_dl(self, url, folder):
         self.cmd(self.YOUTUBE.format(url=url, folder=folder))
@@ -18,17 +21,31 @@ class All_manager(Cmd_manager):
     def exe_pyreverse(self, name_png, file):
         self.cmd(self.PYREVERSE.format(name=name_png, file=file))
 
+    def exe_wget(self, url, folder):
+        self.cmd(self.WGET.format(url=url, folder=folder))
+
+    def exe_feh(self, path):
+        self.cmd(self.FEH.format(pathimg=path))
+
+    def proc_wallpaper(self):
+        string = urlopen('https://api.desktoppr.co/1/wallpapers/random').read()
+        response = json.loads(string)
+        url = response['response']['image']['url']
+        name = url.split('/')[-1]
+        path = "/home/izot/"
+        self.exe_wget(url, path)
+        self.exe_feh(path + name)
+
+
 class All_runer(object):
-
-
     def __init__(self, terminal="sakura", directory='/', file=''):
         self.git_manager = All_manager()
-        self.gui_manager  = Gui_manager()
+        self.gui_manager = Gui_manager()
         self.TERM = terminal
         self.DIR = directory
         self.FILE = file
         self.FILENAME = os.path.basename(self.FILE).split('.')[0] or None
-        self.MENU = {'Youtebe_dl':'ydl', 'Pyreverse':'pyreverse','Menu': ['ydl', 'pyreverse']}
+        self.MENU = {'Youtebe_dl': 'ydl', 'Pyreverse': 'pyreverse', 'Wallpaper':'wallpaper', 'Menu': ['ydl', 'pyreverse', 'wallpaper']}
         self.run()
 
     def choise_execute(self):
@@ -42,8 +59,14 @@ class All_runer(object):
         elif otvet == self.MENU['Pyreverse']:
             self.git_manager.exe_pyreverse(self.FILENAME, self.FILE)
 
+        elif otvet == self.MENU['Wallpaper']:
+            self.git_manager.proc_wallpaper()
+
     def run(self):
         self.choise_execute()
 
+
 if __name__ == '__main__':
     All_runer(directory=sys.argv[1], file=sys.argv[2])
+    # a = All_manager()
+    # a.proc_wallpaper()
